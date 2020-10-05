bf35581dfa65faf07f4a3630815187ea592d40e9
D. Gierveld <d.gierveld@outlook.com>
2020-10-05 13:37:19 +0200

    Copied main.tf and azure pipelines from binarythistle

--- main.tf
+++ main.tf
@@ -4,37 +4,38 @@ provider "azurerm" {
 }
 
 terraform {
-  backend "azurerm" {
-    resource_group_name = "tf_rg_blobstore"
-    storage_account_name = "tfstorageresqtee"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
-  }
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstore"
+        storage_account_name = "tfstorageresqtee"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
 }
 
 variable "imagebuild" {
   type        = string
-  description = "Latest image build"
+  description = "Latest Image Build"
 }
 
 
-resource "azurerm_resource_group" "tf_test"{
-    name = "tfmainrg"
-    location = "West Europe"
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "West Europe"
 }
 
 resource "azurerm_container_group" "tfcg_test" {
-    name ="weatherapi"
-    location = azurerm_resource_group.tf_test.location
-    resource_group_name = azurerm_resource_group.tf_test.name
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
 
-    ip_address_type     = "public"
-    dns_name_label      = "resqteewa"
-    os_type             = "Linux"
-    
-    container {
-        name            = "weatherapi"
-        image           = "resqtee/weatherapi:${var.imagebuild}"
+  ip_address_type     = "public"
+  dns_name_label      = "resqteewa"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "resqtee/weatherapi:${var.imagebuild}"
         cpu             = "1"
         memory          = "1"
 
@@ -42,5 +43,5 @@ resource "azurerm_container_group" "tfcg_test" {
             port        = 80
             protocol    = "TCP"
         }
-    }
+  }
 }
